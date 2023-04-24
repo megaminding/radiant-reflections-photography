6309473620d35d9a7c15204f84acc1aa6d4574c9
megaminding <125762824+megaminding@users.noreply.github.com>
2023-04-24 16:42:58 -0700

    Delete button.js

--- button.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("show-contact");
-const hideContact = document.getElementById("hidden-contact");
-
-button.addEventListener("click", function() {
-    hideContact.style.display = "block";
-});
-
-
-console.log('update')
