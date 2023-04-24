1a447a2b4740e1272c75bd5e6e30ee494e8d5c30
megaminding <125762824+megaminding@users.noreply.github.com>
2023-04-23 21:00:16 -0700

    button

---
+++ button.js
@@ -0,0 +1,9 @@
+const showContact = document.getElementById("show-contact");
+const hideContact = document.getElementById("hidden-contact");
+
+showContact.addEventListener("click", function() {
+    hideContact.style.display = "block";
+});
+
+
+console.log('hi')
