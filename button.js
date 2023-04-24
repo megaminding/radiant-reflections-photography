908ebb1a329529ba798f6ad14ab351e1a131ebbe
megaminding <125762824+megaminding@users.noreply.github.com>
2023-04-23 21:12:25 -0700

    alt

--- button.js
+++ button.js
@@ -1,9 +1,9 @@
-// const showContact = document.getElementById("show-contact");
-// const hideContact = document.getElementById("hidden-contact");
+const button = document.getElementById("show-contact");
+const hideContact = document.getElementById("hidden-contact");
 
-// showContact.addEventListener("click", function() {
-//     hideContact.style.display = "block";
-// });
+button.addEventListener("click", function() {
+    hideContact.style.display = "block";
+});
 
 
 console.log('hi')
